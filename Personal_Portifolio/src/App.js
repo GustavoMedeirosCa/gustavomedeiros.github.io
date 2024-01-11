629f4108d4328aa5966ad5c9140b738e935f7d29
GustavoMedeirosCode <120103818+GustavoMedeirosCa@users.noreply.github.com>
2024-01-11 01:38:04 -0300

    Configurado o ambiente de desenvolvimento react-bootstrap

---
+++ Personal_Portifolio/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css'; 
+import React from 'react';
+import { NavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ Personal_Portifolio/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export const NavBar = () => {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src={''} alt="Logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <span className='navbar-toggler-icon'></span>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#skills">Skills</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
