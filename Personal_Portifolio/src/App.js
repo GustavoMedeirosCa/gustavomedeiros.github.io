087e589cd56785a1794d289db0235794cdb3ca15
GustavoMedeirosCode <120103818+GustavoMedeirosCa@users.noreply.github.com>
2024-01-14 21:01:08 -0300

    Finalizado estilização da NavBar

--- Personal_Portifolio/src/App.js
+++ Personal_Portifolio/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css'; 
 import React from 'react';
-import { NavBar } from './components/NavBar';
+import { NavBar } from "./components/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
